a0b1ce12130bc776578d4f717463aaa719a67b59
Y. Jace Liu <yang.jace.liu@linux.com>
2023-03-17 14:17:06 -0600

    Init
    Signed-off-by: Y. Jace Liu <yang.jace.liu@linux.com>

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+from setuptools import find_packages
+
+setup(name='Bing Chat Discord Bot',
+      version='1.0',
+      description='A discord bot to use Bing Chat',
+      author='Yang Liu',
+      author_email='yang.jace.liu@linux.com',
+      url='https://github.com/yang-jace-liu/bing-chat-bot',
+      packages=find_packages(),
+     )
